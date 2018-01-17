ea14bd7a3dad9896e0089522ca9fe8a3ac715065
leungnamson1023 <leungnamson@gmail.com>
2018-01-17 15:27:29 -0800

    solve functional queue stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,35 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var key = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[key] = value;
+    key++;
   };
 
   someInstance.dequeue = function() {
+    var min;
+
+    for (var key in storage){
+      if(parseInt(key) < min || min === undefined){
+        min = key;
+      }
+    }
+    var deleted = {};
+    deleted[0] = storage[min];
+    delete storage[min];
+    return deleted[0];
   };
 
   someInstance.size = function() {
+    var counter = 0;
+    for (var key in storage){
+      counter++;
+    }
+    return counter;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,33 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var key = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[key] = value;
+    key++;
   };
 
   someInstance.pop = function() {
+    var max;
+    for (var key in storage) {
+      if (parseInt(key) > max || max === undefined) {
+        max = key;
+      }
+    }
+    var deleted = {};
+    deleted[0] = storage[max];
+    delete storage[max];
+    return deleted[0];
   };
 
   someInstance.size = function() {
+    var counter = 0;
+    for (var key in storage) {
+      counter++;
+    }
+    return counter;
   };
 
   return someInstance;
