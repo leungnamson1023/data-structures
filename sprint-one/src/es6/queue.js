913ebaafa3b2c48ac122dfce2b4149717b83772f
leungnamson1023 <leungnamson@gmail.com>
2018-01-17 19:59:23 -0800

    "passed tests for ES6"

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,35 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.storage.items = {};
+    this.storage.key = 0;
+  }
+
+  enqueue(value){
+    this.storage.items[this.storage.key] = value;
+    this.storage.key++;
+  }
+  
+  dequeue(){
+    var min;
+    for (var key in this.storage.items){
+      if(parseInt(key) < min || min === undefined ) {
+        min = key;
+      }
+    }
+    var deleted = {};
+    deleted[0] = this.storage.items[min];
+    delete this.storage.items[min];
+    return deleted[0];
+  }
+
+  size(){
+    var counter = 0;
+    for (var key in this.storage.items){
+      counter++;
+    }
+    return counter;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,37 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.storage.key = 0;
+    this.storage.items = {};
+  }
+
+
+  push(value) {
+    this.storage.items[this.storage.key] = value;
+    this.storage.key++;
+  }
+
+
+  pop() {
+    var max;
+    for (var key in this.storage.items) {
+      if (parseInt(key) > max || max === undefined) {
+        max = key;
+      }
+    }
+    var deleted = {};
+    deleted[0] = this.storage.items[max];
+    delete this.storage.items[max];
+    return deleted[0];
+  }
+
+  size() {
+    var count = 0;
+    for (var key in this.storage.items) {
+      count++;
+    }
+    return count;
   }
 
 }
