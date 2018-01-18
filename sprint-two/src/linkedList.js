964099809186426ce33c5d20fe9d7dc4f7f00ae0
leungnamson1023 <leungnamson@gmail.com>
2018-01-18 15:58:31 -0800

    solve linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,50 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  
+
+
+
+
+
+  list.findTail = function(someNode, value){
+    if(someNode.next !== null) {
+      return findTail(someNode.next);
+    } else {
+      someNode.next = Node(value);
+      list.tail = someNode.next;
+    }
+  };
+
+  list.findValue = function(node, target){
+    if (node.value !== target && node.next !== null){
+      return list.findValue(node.next, target);
+    } else if(node.value === target){
+      return true;
+    }  
+    return false;
+  };
 
   list.addToTail = function(value) {
+
+    if (list.head === null){
+      list.head = Node(value);
+
+      list.tail = list.head;
+
+    } else {
+      list.findTail(list.head, value);
+    }
   };
 
   list.removeHead = function() {
+    var deletedHeadValue = list.head.value;
+    list.head = list.head.next;
+    return deletedHeadValue;
   };
 
   list.contains = function(target) {
+    return list.findValue(list.head, target);
   };
 
   return list;
