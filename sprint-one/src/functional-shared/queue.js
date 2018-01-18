46586c0c81038ed50c71ef495e280be9a158f081
leungnamson1023 <leungnamson@gmail.com>
2018-01-17 17:26:14 -0800

    solve funcional shared stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,39 @@
 var Queue = function() {
+  var storage = {};
+  storage.itemCount = 0;
+  storage.items = {};
+  
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  _.extend(storage, queueMethods);
+  return storage;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value){
+  this.items[this.itemCount] = value;
+  this.itemCount++;
+};
 
+queueMethods.dequeue = function(){
+  var min;
+  for (var key in this.items){
+    if (parseInt(key) < min || min === undefined){
+      min = key;
+    } 
+  }
+  var deleted = {};
+  deleted[0] = this.items[min];
+  delete this.items[min];
+  return deleted[0];
+};
+
+queueMethods.size = function(){
+  var counter = 0;
+  for (var key in this.items){
+    console.log(key, 'keys');
+    counter++;
+  }
+  return counter;
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,39 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  storage.key = 0;
+  storage.items = {};
+  
+  _.extend(storage, stackMethods);
+
+  return storage;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.items[this.key] = value;
+  this.key++;
+};
 
+stackMethods.pop = function(){
+  var max;
+  for (var key in this.items){
+    if(parseInt(key) > max || max === undefined) {
+      max = key;
+    }
+  }
+  var deleted = {};
+  deleted[0] = this.items[max];
+  delete this.items[max];
+  return deleted[0];
+};
+
+stackMethods.size = function(){
+  var counter = 0;
+  for (var key in this.items){
+    counter++;
+  }
+  return counter;
+};
