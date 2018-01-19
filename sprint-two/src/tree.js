864ac3acb7590c01521fa81f4dfab81d0cb8a476
leungnamson1023 <leungnamson@gmail.com>
2018-01-18 18:41:01 -0800

    solve tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,20 +3,57 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
+
+// treeMethods.contains = function(target, parent) {
+//   if (arguments.length < 2) {
+//     var parent = this;
+//   }
+//   if (this.children.length === 0) {
+//     return false;
+//   } else if (this.value === target) {
+//     return true;
+//   } else if (this.children.length > 0) {
+//     console.log(this);
+//     for (var i = 0; i < this.children.length; i++) {
+//       return this.contains(target, this.children[i]);
+//     }
+//     return false;
+//   } 
+// };
 
 treeMethods.contains = function(target) {
+  var result = false;
+
+  var recursion = function(parent) {
+    if (parent.value === target) {
+      result = true;
+    } else if (parent.children.length > 0) {
+      for (var i = 0; i < parent.children.length; i++) {
+        console.log(parent.children[i]);
+        if (parent.children[i].value === target) {
+          result = true;
+        }
+        recursion(parent.children[i]);
+      }
+    } 
+  };
+  recursion(this);
+  return result;
 };
 
-
+// treeMethods.getChild = function(parent) {
+//   return parent.children
+// };
 
 /*
  * Complexity: What is the time complexity of the above functions?
